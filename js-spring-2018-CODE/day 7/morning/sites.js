d5c71c6084e67728d23e1e0751abeec386734010
Waleed Rawasia <35583388+Waldoz53@users.noreply.github.com>
2018-02-21 13:37:52 -0700

    day 7 complete!

---
+++ js-spring-2018-CODE/day 7/morning/sites.js
@@ -0,0 +1,32 @@
+myFunction([
+{
+"display": "Google",
+"url": "http://google.ca",
+"title": "go to google.ca"
+},
+{
+"display": "Yahoo",
+"url": "http://yahoo.ca",
+"title": "go to yahoo.ca"
+},
+{
+"display": "Bing",
+"url": "http://bing.ca",
+"title": "go to Bing"
+},
+{
+"display": "DZone.com",
+"url": "http://DZone.com",
+"title": "go to DZone.com"
+},
+{
+"display": "StackOverflow",
+"url": "http://stackoverflow.com",
+"title": "go to stackoverflow"
+},
+{
+"display": "MSN",
+"url": "http://msn.ca",
+"title": "go to MSN.ca"
+}
+])
